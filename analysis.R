98c11cd985d9d89c27dd2f59188a3dd5144c3e9c
Sandy Chien <sandy617@cs.washington.edu>
2020-11-16 12:00:58 -0800

    Make plots on vote difference

--- analysis.R
+++ analysis.R
@@ -10,10 +10,25 @@ num_rows <- nrow(data)
 num_states <- length(unique(data$state))
 num_timestamps <- length(unique(data$timestamp))
 
-# Formatting: splitting out state name from electoral votes
+# Formatting:
+# Splitting out state name from electoral votes
+# Add Biden and Trump vote columns
 data <- raw_data %>%
   separate(state, into=c("state", "ev"), " \\(") %>%
-  mutate(ev = parse_number(ev))
+  mutate(ev = parse_number(ev)) %>%
+  mutate(biden_votes =
+           if_else(leading_candidate_name == "Biden", # condition
+                   leading_candidate_votes,           # if true
+                   trailing_candidate_votes,          #if false
+                   ),
+         trump_votes = total_votes_count - biden_votes
+         )
+
+# Quick check
+check <- data %>%
+  mutate(total_check = trump_votes + biden_votes,
+         done_correctly = if_else(total_check == total_votes_count, 1, 0)) %>%
+  summarize(total_correct = sum(done_correctly))
 
 # The number of time stamps varies for each state.
 # How many reported time stamps exist for each state?
@@ -27,8 +42,36 @@ ga_lead_time <- data %>%
   filter(timestamp == min(timestamp)) %>%
   pull(timestamp)
 
-# What is the difference in votes in each state?
+# What is the earliest time in each state that Biden is ahead?
+biden_lead_time <- data %>%
+  group_by(state) %>%
+  filter(leading_candidate_name == "Biden") %>%
+  filter(timestamp == min(timestamp)) %>%
+  select(state, timestamp)
 
+# What is the difference in votes in each state?
+# (at the most recent time stamp)
+vote_diff <- data %>%
+  group_by(state) %>%
+  filter(timestamp == max(timestamp)) %>%
+  mutate(vote_diff = biden_votes - trump_votes,
+         pct_diff = vote_diff / total_votes_count)
+
+vote_diff_plot <- ggplot(vote_diff) +
+  geom_col(mapping = aes(x = vote_diff,
+                         y = reorder(state, vote_diff),
+                         fill = leading_candidate_name)) +
+  scale_fill_manual(values = c("blue", "red")) +
+  labs(x = "Vote Difference",
+       y = "State",
+       fill = "Candidate",
+       title = "Vote Difference at Most Recent Time Stamp")
+
+vote_pct_plot <- ggplot(vote_diff) +
+  geom_col(mapping = aes(x = pct_diff,
+                         y = reorder(state, pct_diff),
+                         fill = leading_candidate_name)) +
+  scale_fill_manual(values = c("blue", "red"))
 
 # How do total votes change over time (by candidate)?
 
